29940401e5a3d2e7db1641f5c340587d72d453a3
v1tso <v.renasco.larrain@gmail.com>
2022-07-31 12:44:34 -0400

    Added logs on the console, there is an error that the program does not handle two+ word crytocurrencies.

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from click import command
 import telebot
 from pycoingecko import CoinGeckoAPI
 from coins import coins
+from newsapi import NewsApiClient
+import time
 
+
+news = NewsApiClient(api_key='5753feb0897a4ff5a322ccd4289acede')
 coin_client = CoinGeckoAPI()
 API_KEY = os.environ.get('API_KEY')
 bot = telebot.TeleBot("5587433203:AAEvScGpJH8VuCzwB0jaD6peNTru-P67X6k")
@@ -13,17 +17,44 @@ bot = telebot.TeleBot("5587433203:AAEvScGpJH8VuCzwB0jaD6peNTru-P67X6k")
 def info(message):
     if len(message.text.strip().split()) == 2:
         coin = message.text.strip(' ').split()[1]
+        print(f"The user requested info about {coin}:")
+        print()
         if coin in coins:
             info = coin_client.get_coin_by_id(id=coin,localization='false', tickers='true', market_data='true', community_data='false', developer_data='false', sparkline='false')
             if info:
                 current_price = info['market_data']['current_price']['usd']
                 bot.reply_to(message, text=f'Price: {current_price}')
+                print(f'Price: {current_price}')
+                print()
+                print('------------------------------------------------------')
+                print()
         else:
             bot.reply_to(message, text=f'Error, {coin} was not found')
+            print(f'Error, {coin} was not found')
+            print()
+            print('------------------------------------------------------')
+            print()
     else:
         bot.reply_to(message, text='Error, please use /info <coin>')
+        print('Error, please use /info <coin>')
+        print(f"Number of args: {len(message.text.strip().split())}")
+        print()
+        print('------------------------------------------------------')
+        print()
 
 
 
 if __name__ == '__main__':
+    print('Initializing bot...')
+    time.sleep(2)
+    print('Bot is running...')
+    time.sleep(2)
+    print('waiting for commands...')
+    time.sleep(2)
+    print('available commands: /info <coin>')
+    print('Press Ctrl+C to stop')
+    print('Logs:')
+    print()
+    print('------------------------------------------------------')
+    print()
     bot.polling()
